b8a58d1fcc207ae4f9f8a3d50c8e749be414a34a
sys-wanto <nckdostrow@gmail.com>
2023-07-27 23:02:29 +0700

    feat : ajax to flask

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -21,7 +21,7 @@
             <div class="row align-items-start">
                 <div class="col-12">
                     <div class="d-flex align-items-center justify-content-center w-100">
-                        <textarea name="kronologi" id="" cols="30" rows="10" class="w-100"></textarea>
+                        <textarea name="kronologi" id="kronologi" cols="30" rows="10" class="w-100"></textarea>
                     </div>
                 </div>
                 <div class="col-12">
@@ -29,29 +29,32 @@
                         <div class="col-12 m-2">
                             <div class="row">
                                 <div class="col w-100 m-2">
-                                    <button type="button" name="preprocessing"
-                                        class="btn btn-primary w-100">PRE-PROCESSING</button>
+                                    <button type="button" name="preprocessing" class="btn btn-primary w-100"
+                                        id="preprocessing">PRE-PROCESSING</button>
                                 </div>
                                 <div class="col w-100 m-2">
-                                    <button type="button" name="kmeans" class="btn btn-primary w-100">K-MEANS</button>
+                                    <button type="button" name="kmeans" class="btn btn-primary w-100"
+                                        id="kmeans">K-MEANS</button>
                                 </div>
                             </div>
                         </div>
                         <div class="col-12 m-2">
                             <div class="row">
                                 <div class="col w-50 m-2">
-                                    <button type="button" name="tfidf" class="btn btn-primary w-100">TF-IDF</button>
+                                    <button type="button" name="tfidf" class="btn btn-primary w-100"
+                                        id="tfidf">TF-IDF</button>
                                 </div>
                                 <div class="col w-50 m-2">
-                                    <button type="button" name="evaluasi"
-                                        class="btn btn-primary w-100">EVALUASI</button>
+                                    <button type="button" name="evaluasi" class="btn btn-primary w-100"
+                                        id="evaluasi">EVALUASI</button>
                                 </div>
                             </div>
                         </div>
                         <div class="col-12 m-2">
                             <div class="row">
                                 <div class="col w-50 m-2">
-                                    <button type="button" name="hasil" class="btn btn-primary w-100">HASIL</button>
+                                    <button type="button" name="hasil" class="btn btn-primary w-100"
+                                        id="hasil">HASIL</button>
                                 </div>
                             </div>
                         </div>
@@ -60,6 +63,8 @@
             </div>
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"
+        integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
     </script>
@@ -68,6 +73,27 @@
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"
         integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous">
+    </script>
+    <script type="text/javascript">
+        $('#tfidf').click(function() {
+            var kronologi = $('textarea#kronologi').val();
+            var splited = kronologi.split('\n');
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:5000/tfidf",
+                data: {
+                    q: splited
+                },
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                },
+                success: callbackFunc
+            });
+        });
+
+        function callbackFunc(response) {
+            console.log(response);
+        }
     </script>
 </body>
 
